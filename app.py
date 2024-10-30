a8761424ab35b1661f8d31d7b018a8e5881daa00
Owen Treanor <realawesomebeary@gmail.com>
2024-10-30 01:52:24 -0400

    Navbar, base.html, and some cleanup

--- app.py
+++ app.py
@@ -3,59 +3,30 @@ from calculate import *
 
 app = Flask(__name__)
 
-onResult = False
-pokemon_name = ""
-variety_list = []
-variety = 0
-fromPost = False
+pokemonName = ""
+varietyList = []
 
-# typeImages = {
-#         'normal': 'normal.png',
-#         'fire': 'fire.png',
-#         'water': 'water.png',
-#         'electric': 'electric.png',
-#         'grass': 'grass.png',
-#         'ice': 'ice.png',
-#         'fighting': 'fighting.png',
-#         'poison': 'poison.png',
-#         'ground': 'ground.png',
-#         'flying': 'flying.png',
-#         'psychic': 'psychic.png',
-#         'bug': 'bug.png',
-#         'rock': 'rock.png',
-#         'ghost': 'ghost.png',
-#         'dragon': 'dragon.png',
-#         'dark': 'dark.png',
-#         'steel': 'steel.png',
-#         'fairy': 'fairy.png'
-# }
-
-@app.route('/result/<pokemon_name>', methods = ['GET', 'POST'])
-def result(pokemon_name):
-    global fromPost
-    if fromPost:
-        fromPost = False
-        return render_template('result.html', varietyList = variety_list, index = 0)
-    else:
-        return redirect(url_for('index'))
+@app.route('/result/<pokemonName>', methods = ['GET', 'POST'])
+def result(pokemonName):
+        return render_template('result.html', styleSheet = "resultStyles.css", varietyList = varietyList, index = 0)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    global onResult, pokemon_name, variety_list, variety, fromPost
+    global pokemonName, varietyList
     if request.method == 'POST':
-        if not onResult:
-            onResult = True
-            pokemon_name = request.form['pokemon_name']
-            variety_list = getEverything(pokemon_name)
-            if variety_list:
-                fromPost = True
-                return redirect(url_for('result', pokemon_name = pokemon_name))
-            else:
-                onResult = False
-                return render_template('index.html', error="Pokemon not found!")
+        pokemonName = request.form['pokemonName']
+        varietyList = getEverything(pokemonName)
+        if varietyList:
+            return redirect(url_for('result', pokemonName = pokemonName))
+        else:
+            return render_template('index.html', styleSheet = "indexStyles.css", error="Pokemon not found!")
+    return render_template('index.html', styleSheet = "indexStyles.css")
 
-    onResult = False        
-    return render_template('index.html')
+# NEXT TASK -- MAKE AN ERROR PAGE OR SESSION
+
+@app.route('/about', methods=['GET'])
+def about():
+    return render_template("about.html", styleSheet = "aboutStyles.css")
 
 
 if __name__ == '__main__':
