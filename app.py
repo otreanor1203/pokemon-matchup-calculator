be39278f940796340cee06f994781e104c83df3d
Owen Treanor <realawesomebeary@gmail.com>
2024-11-09 23:14:12 -0500

    Started working on styles for the home page

--- app.py
+++ app.py
@@ -1,12 +1,18 @@
-from flask import Flask, redirect, request, render_template, url_for
+from flask import Flask, jsonify, redirect, request, render_template, url_for
 from calculate import *
 
 app = Flask(__name__)
+
+f = open('names.txt', 'r')
+allPokemonNames = [line.strip() for line in f]
     
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        return redirect(url_for('result', pokemonName = request.form['homeSearch']))
+        pokemonName = request.form['homeSearch']
+        if not pokemonName:
+            return redirect(url_for('error', pokemonName = "null"))
+        return redirect(url_for('result', pokemonName = pokemonName))
     return render_template('index.html', isIndex = True)
 
 @app.route('/result/<pokemonName>', methods = ['GET', 'POST'])
@@ -29,6 +35,12 @@ def error(pokemonName):
     if request.method == 'POST':
         return redirect(url_for('result', pokemonName = request.form['navSearch']))
     return render_template('error.html')
+
+@app.route('/autocomplete')
+def autocomplete():
+    query = request.args.get('query', '').lower()
+    matches = [name for name in allPokemonNames if name.lower().startswith(query)]
+    return jsonify(matches[:5])
 
 
 if __name__ == '__main__':
--- getInvalid.py
+++ getInvalid.py
@@ -6,18 +6,15 @@ namesList = file.readlines()
 
 invalidNames = open('invalidNames.txt', 'w')
 
-# print(namesList)
 
 for name in namesList:
     data = getPokemonData(name[:-1])
     if not data:
         print(f"{name[:-1].upper()} DID NOT WORK!!!!!!!!!!")
+        invalidNames.write(f"{name}")
     else:
         type = getType(data[0])
-        if not type:
-            print(f"{name[:-1].upper()} DID NOT WORK!!!!!!!!!!")
-            invalidNames.write(f"{name}")
-        elif type == ['variety']:
+        if type == ['variety']:
             print(f"{name[:-1]} has varieties!")
             invalidNames.write(f"{name}")
         else:
